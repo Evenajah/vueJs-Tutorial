cf041a000efe2d69f22050ee21eb3a8d48e0ef89
Evenajah <evelastest@gmail.com>
2022-01-25 10:32:16 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-uhfe9z
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-uhfe9z)
---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <input v-model.number="age" type="number" v-model="person" />
+    <button @click="shoPerson()">Click me</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: function () {
+    return {
+      person: '20',
+    };
+  },
+  methods: {
+    shoPerson() {
+      alert(this.person);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
